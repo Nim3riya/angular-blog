10c2d17396c3127e19e050dab425e995e2535fb0
Nim3riya <46938740+Nim3riya@users.noreply.github.com>
2023-03-15 23:23:17 +0300

    design of the post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomePageComponent } from './home-page/home-page.component';
     MainLayoutComponent,
     PostPageComponent,
     HomePageComponent,
+    PostComponent,
 
   ],
   imports: [
